bb926500877d3f320408354d5b79c958286ab1bb
PB-BZH <patrick.bourges@univ-brest.fr>
2026-07-14 17:32:28 +0200

    Ajout d'un variant de collectible animé

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 public sealed class Collectible: MonoBehaviour {
   public event Action<Collectible> Collected;
 
+  [Header("Animation")]
+  [SerializeField]
+  [Min(0f)]
+  private float rotationSpeed = 90f;
+
   private bool _isCollected;
+
+  private void Update() {
+    transform.Rotate(
+        xAngle: 0f,
+        yAngle: 0f,
+        zAngle: rotationSpeed * Time.deltaTime);
+  }
 
   private void Reset() {
     Collider2D collectibleCollider = GetComponent<Collider2D>();
@@ -13,11 +25,15 @@ public sealed class Collectible: MonoBehaviour {
   }
 
   private void OnTriggerEnter2D(Collider2D other) {
-    if (_isCollected || !other.TryGetComponent<PlayerController>(out _)) {
+    if (_isCollected ||
+        !other.TryGetComponent<PlayerController>(out _)) {
       return;
     }
+
     _isCollected = true;
+
     Collected?.Invoke(this);
+
     Destroy(gameObject);
   }
 }
