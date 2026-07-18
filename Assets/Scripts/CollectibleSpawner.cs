4771e3bb727fa79bce5e7eb0542a8ea0ea7929d2
PB-BZH <patrick.bourges@univ-brest.fr>
2026-07-18 20:10:43 +0200

    Génération dynamique des collectibles du niveau 4

---
+++ Assets/Scripts/CollectibleSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[DefaultExecutionOrder(-50)]
+[DisallowMultipleComponent]
+public sealed class CollectibleSpawner: MonoBehaviour {
+  [Header("Collectible")]
+  [SerializeField]
+  private Collectible collectiblePrefab;
+
+  [Header("Points d'apparition")]
+  [SerializeField]
+  private Transform[] spawnPoints;
+
+  private void Awake() {
+    if (collectiblePrefab == null) {
+      Debug.LogError("CollectibleSpawner : aucun prefab de collectible n'est renseigné.",this);
+      enabled = false;
+      return;
+    }
+
+    foreach (Transform spawnPoint in spawnPoints) {
+      if (spawnPoint == null) {
+        continue;
+      }
+      Instantiate(collectiblePrefab,spawnPoint.position,spawnPoint.rotation,transform);
+    }
+  }
+}
