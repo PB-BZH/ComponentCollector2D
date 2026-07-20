c712f6706ec9eaa158f171b07d32b21911b61592
PB-BZH <patrick.bourges@univ-brest.fr>
2026-07-20 20:54:03 +0200

    Ajoute le tri vertical et l éclairage dynamique 2D

---
+++ Assets/Scripts/YSortRenderer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public sealed class YSortRenderer: MonoBehaviour {
+  [Header("Rendu")]
+  [SerializeField]
+  private SpriteRenderer targetRenderer;
+
+  [Header("Tri vertical")]
+  [SerializeField]
+  [Min(1)]
+  private int precision = 100;
+
+  [SerializeField]
+  private int orderOffset;
+
+  private void Reset() {
+    targetRenderer = GetComponentInChildren<SpriteRenderer>();
+  }
+
+  private void Awake() {
+    if (targetRenderer == null) {
+      targetRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    if (targetRenderer == null) {
+      Debug.LogError("YSortRenderer : aucun SpriteRenderer n'a été trouvé.",this);
+      enabled = false;
+      return;
+    }
+
+    UpdateSortingOrder();
+  }
+
+  private void LateUpdate() {
+    UpdateSortingOrder();
+  }
+
+  private void UpdateSortingOrder() {
+    targetRenderer.sortingOrder = orderOffset - Mathf.RoundToInt(transform.position.y * precision);
+  }
+}
